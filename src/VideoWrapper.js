16906dea0fe1fdef410bd736df8dfacf63b2444b
Ziaul Hasan <ziaul@cefalo.com>
2018-05-21 17:06:07 +0600

    Added flex:1 to video component

--- src/VideoWrapper.js
+++ src/VideoWrapper.js
@@ -83,7 +83,7 @@ export class VideoWrapper extends Component {
         onError={this.onError}
         onBuffer={this.onBuffer}
         onTimedMetadata={this.onTimedMetadata}
-        style={{position: 'absolute', top: 0, right: 0, bottom: 0, left: 0}}
+        style={{flex: 1, position: 'absolute', top: 0, right: 0, bottom: 0, left: 0}}
       />
     )
   }
